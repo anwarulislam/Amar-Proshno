e6df4d994c7fd7f1490ba696f11a12b218b01af2
Anwarul Islam <aib.tmkm@gmail.com>
2018-06-26 19:56:08 +0600

    sql addition

---
+++ amarproshno.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2018 at 03:50 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amarproshno`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+-- Error reading structure for table amarproshno.answers: #1932 - Table 'amarproshno.answers' doesn't exist in engine
+-- Error reading data for table amarproshno.answers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`answers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+-- Error reading structure for table amarproshno.questions: #1932 - Table 'amarproshno.questions' doesn't exist in engine
+-- Error reading data for table amarproshno.questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`questions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table amarproshno.users: #1932 - Table 'amarproshno.users' doesn't exist in engine
+-- Error reading data for table amarproshno.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
