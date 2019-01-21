70a0bdd0b989192aed8cc8f51d7842d47083fce6
Anwarul Islam <anwaarulislaam@gmail.com>
2019-01-21 16:28:39 +0600

    updated sql and others stuff

--- amarproshno.sql
+++ amarproshno.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2018 at 03:50 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Jan 21, 2019 at 11:27 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,24 +27,117 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `answers`
 --
--- Error reading structure for table amarproshno.answers: #1932 - Table 'amarproshno.answers' doesn't exist in engine
--- Error reading data for table amarproshno.answers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`answers`' at line 1
+
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `question_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `user_id`, `title`, `description`, `created_at`, `question_id`) VALUES
+(1, 2, '', '<p>hello brother how are you?</p>\r\n', '2019-01-21 11:01:51', 1);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `questions`
 --
--- Error reading structure for table amarproshno.questions: #1932 - Table 'amarproshno.questions' doesn't exist in engine
--- Error reading data for table amarproshno.questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`questions`' at line 1
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `user_id`, `title`, `description`, `created_at`) VALUES
+(1, 2, 'How to change URL parameter and load dynamic content with that by dropdown value in Angular?', '<p>asdfasdfasdfa asdf asdfa sadf asdf asdf asdf asdf asdf asdf</p>\r\n', '2019-01-21 11:01:39');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
--- Error reading structure for table amarproshno.users: #1932 - Table 'amarproshno.users' doesn't exist in engine
--- Error reading data for table amarproshno.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `amarproshno`.`users`' at line 1
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middle_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `hobby` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_admin` int(11) NOT NULL,
+  `gender` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `interest` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_birth` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `email`, `password`, `created_at`, `hobby`, `photo`, `is_admin`, `gender`, `interest`, `date_of_birth`) VALUES
+(1, 'Anwarul', 'bablu', 'Islam', 'anwaarulislaam@gmail.com', '651830', '2019-01-21 11:01:56', '', '', 0, '', '', '0000-00-00'),
+(2, 'Anwarul', 'Islam', 'Bablu', 'aib.tmkm@gmail.com', '651830', '2019-01-21 11:01:11', '', 'assets/images/user.jpg', 1, 'Male', '', '0000-00-00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
